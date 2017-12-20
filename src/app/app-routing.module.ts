8ad91cded2f07755276e4c00bf66778d306bf196
sushildangi <dangi.sushil5@gmail.com>
2017-12-21 00:48:54 +0530

    guards canDeactivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {UsersComponent} from './users/users.component';
 import {UserComponent} from './users/user/user.component';
 import {AuthGuard} from './auth-guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -23,7 +24,7 @@ const appRoutes: Routes = [
     component: ServersComponent,
     children: [
       {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},
+      {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
     ]
   },
   {path: 'not-found', component: PageNotFoundComponent},
