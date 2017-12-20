56814e95abbde2359ef767e07fc2dfdd5e2032a1
sushildangi <dangi.sushil5@gmail.com>
2017-12-20 23:44:55 +0530

    separate App routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,9 @@ import {ServerComponent} from './servers/server/server.component';
 import {UserComponent} from './users/user/user.component';
 import {FormsModule} from '@angular/forms';
 import {ServersService} from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent},
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},
-    ]
-  },
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'},
-
-];
 
 @NgModule({
   declarations: [
@@ -46,7 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule,
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
